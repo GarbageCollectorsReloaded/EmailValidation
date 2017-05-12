5bd8382a1ca86c810c515956cf8768ada1413072
Princz, Ákos <elminainos@users.noreply.github.com>
2017-05-12 19:19:10 +0200

    Code coverage complete (#16)
    * CompletCodeCoverage
    
    Finished code coverage to reach 100%.
    
    * fixed tabs

--- UnitTests/Test.cs
+++ UnitTests/Test.cs
@@ -33,240 +33,243 @@ using System.Reflection;
 [assembly: AssemblyVersionAttribute("1.0.0.0")]
 namespace UnitTests
 {
-	[TestFixture]
-	public class Test
-	{
-		static readonly string[] ValidAddresses = {
-			"\"Abc\\@def\"@example.com",
-			"\"Fred Bloggs\"@example.com",
-			"\"Joe\\\\Blow\"@example.com",
-			"\"Abc@def\"@example.com",
-			"customer/department=shipping@example.com",
-			"$A12345@example.com",
-			"!def!xyz%abc@example.com",
-			"_somename@example.com",
-			"valid.ipv4.addr@[123.1.72.10]",
-			"valid.ipv6.addr@[IPv6:0::1]",
-			"valid.ipv6.addr@[IPv6:2607:f0d0:1002:51::4]",
-			"valid.ipv6.addr@[IPv6:fe80::230:48ff:fe33:bc33]",
-			"valid.ipv6.addr@[IPv6:fe80:0000:0000:0000:0202:b3ff:fe1e:8329]",
-			"valid.ipv6v4.addr@[IPv6:aaaa:aaaa:aaaa:aaaa:aaaa:aaaa:127.0.0.1]",
+    [TestFixture]
+    public class Test
+    {
+        static readonly string[] ValidAddresses = {
+            "\"Abc\\@def\"@example.com",
+            "\"Fred Bloggs\"@example.com",
+            "\"Joe\\\\Blow\"@example.com",
+            "\"Abc@def\"@example.com",
+            "customer/department=shipping@example.com",
+            "$A12345@example.com",
+            "!def!xyz%abc@example.com",
+            "_somename@example.com",
+            "valid.ipv4.addr@[123.1.72.10]",
+            "valid.ipv6.addr@[IPv6:0::1]",
+            "valid.ipv6.addr@[IPv6:2607:f0d0:1002:51::4]",
+            "valid.ipv6.addr@[IPv6:fe80::230:48ff:fe33:bc33]",
+            "valid.ipv6.addr@[IPv6:fe80:0000:0000:0000:0202:b3ff:fe1e:8329]",
+            "valid.ipv6v4.addr@[IPv6:aaaa:aaaa:aaaa:aaaa:aaaa:aaaa:127.0.0.1]",
 
-			// examples from wikipedia
-			"niceandsimple@example.com",
-			"very.common@example.com",
-			"a.little.lengthy.but.fine@dept.example.com",
-			"disposable.style.email.with+symbol@example.com",
-			"user@[IPv6:2001:db8:1ff::a0b:dbd0]",
-			"\"much.more unusual\"@example.com",
-			"\"very.unusual.@.unusual.com\"@example.com",
-			"\"very.(),:;<>[]\\\".VERY.\\\"very@\\\\ \\\"very\\\".unusual\"@strange.example.com",
-			"postbox@com",
-			"admin@mailserver1",
-			"!#$%&'*+-/=?^_`{}|~@example.org",
-			"\"()<>[]:,;@\\\\\\\"!#$%&'*+-/=?^_`{}| ~.a\"@example.org",
-			"\" \"@example.org",
+            // examples from wikipedia
+            "niceandsimple@example.com",
+            "very.common@example.com",
+            "a.little.lengthy.but.fine@dept.example.com",
+            "disposable.style.email.with+symbol@example.com",
+            "user@[IPv6:2001:db8:1ff::a0b:dbd0]",
+            "\"much.more unusual\"@example.com",
+            "\"very.unusual.@.unusual.com\"@example.com",
+            "\"very.(),:;<>[]\\\".VERY.\\\"very@\\\\ \\\"very\\\".unusual\"@strange.example.com",
+            "postbox@com",
+            "admin@mailserver1",
+            "!#$%&'*+-/=?^_`{}|~@example.org",
+            "\"()<>[]:,;@\\\\\\\"!#$%&'*+-/=?^_`{}| ~.a\"@example.org",
+            "\" \"@example.org",
 
-			// examples from https://github.com/Sembiance/email-validator
-			"\"\\e\\s\\c\\a\\p\\e\\d\"@sld.com",
-			"\"back\\slash\"@sld.com",
-			"\"escaped\\\"quote\"@sld.com",
-			"\"quoted\"@sld.com",
-			"\"quoted-at-sign@sld.org\"@sld.com",
-			"&'*+-./=?^_{}~@other-valid-characters-in-local.net",
-			"01234567890@numbers-in-local.net",
-			"a@single-character-in-local.org",
-			"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
-			"backticksarelegit@test.com",
-			"bracketed-IP-instead-of-domain@[127.0.0.1]",
-			"country-code-tld@sld.rw",
-			"country-code-tld@sld.uk",
-			"letters-in-sld@123.com",
-			"local@dash-in-sld.com",
-			"local@sld.newTLD",
-			"local@sub.domains.com",
-			"mixed-1234-in-{+^}-local@sld.net",
-			"one-character-third-level@a.example.com",
-			"one-letter-sld@x.org",
-			"punycode-numbers-in-tld@sld.xn--3e0b707e",
-			"single-character-in-sld@x.org",
-			"the-character-limit@for-each-part.of-the-domain.is-sixty-three-characters.this-is-exactly-sixty-three-characters-so-it-is-valid-blah-blah.com",
-			"the-total-length@of-an-entire-address.cannot-be-longer-than-two-hundred-and-fifty-four-characters.and-this-address-is-254-characters-exactly.so-it-should-be-valid.and-im-going-to-add-some-more-words-here.to-increase-the-length-blah-blah-blah-blah-bla.org",
-			"uncommon-tld@sld.mobi",
-			"uncommon-tld@sld.museum",
-			"uncommon-tld@sld.travel",
-		};
+            // examples from https://github.com/Sembiance/email-validator
+            "\"\\e\\s\\c\\a\\p\\e\\d\"@sld.com",
+            "\"back\\slash\"@sld.com",
+            "\"escaped\\\"quote\"@sld.com",
+            "\"quoted\"@sld.com",
+            "\"quoted-at-sign@sld.org\"@sld.com",
+            "&'*+-./=?^_{}~@other-valid-characters-in-local.net",
+            "01234567890@numbers-in-local.net",
+            "a@single-character-in-local.org",
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
+            "backticksarelegit@test.com",
+            "bracketed-IP-instead-of-domain@[127.0.0.1]",
+            "country-code-tld@sld.rw",
+            "country-code-tld@sld.uk",
+            "letters-in-sld@123.com",
+            "local@dash-in-sld.com",
+            "local@sld.newTLD",
+            "local@sub.domains.com",
+            "mixed-1234-in-{+^}-local@sld.net",
+            "one-character-third-level@a.example.com",
+            "one-letter-sld@x.org",
+            "punycode-numbers-in-tld@sld.xn--3e0b707e",
+            "single-character-in-sld@x.org",
+            "the-character-limit@for-each-part.of-the-domain.is-sixty-three-characters.this-is-exactly-sixty-three-characters-so-it-is-valid-blah-blah.com",
+            "the-total-length@of-an-entire-address.cannot-be-longer-than-two-hundred-and-fifty-four-characters.and-this-address-is-254-characters-exactly.so-it-should-be-valid.and-im-going-to-add-some-more-words-here.to-increase-the-length-blah-blah-blah-blah-bla.org",
+            "uncommon-tld@sld.mobi",
+            "uncommon-tld@sld.museum",
+            "uncommon-tld@sld.travel",
+        };
 
-		static readonly string[] InvalidAddresses = {
-			"",
-			"invalid",
-			"invalid@",
-			"invalid @",
-			"invalid@[555.666.777.888]",
-			"invalid@[IPv6:123456]",
-			"invalid@[127.0.0.1.]",
-			"invalid@[127.0.0.1].",
-			"invalid@[127.0.0.1]x",
+        static readonly string[] InvalidAddresses = {
+            "",
+            "\"",
+            "\"é129.185.11.100",
+            "invalid@[127.0.0.1",
 
-			// examples from wikipedia
-			"Abc.example.com",
-			"A@b@c@example.com",
-			"a\"b(c)d,e:f;g<h>i[j\\k]l@example.com",
-			"just\"not\"right@example.com",
-			"this is\"not\\allowed@example.com",
-			"this\\ still\\\"not\\\\allowed@example.com",
+            "invalid",
+            "invalid@",
+            "invalid @",
+            "invalid@[555.666.777.888]",
+            "invalid.ipv4.addr@[123]",
+            "invalid@[IPv6:123456]",
+            "invalid@[127.0.0.1.]",
+            "invalid@[127.0.0.1].",
+            "invalid@[127.0.0.1]x",
+            
 
-			// examples from https://github.com/Sembiance/email-validator
-			"! #$%`|@invalid-characters-in-local.org",
-			"(),:;`|@more-invalid-characters-in-local.org",
-			"* .local-starts-with-dot@sld.com",
-			"<>@[]`|@even-more-invalid-characters-in-local.org",
-			"@missing-local.org",
-			"IP-and-port@127.0.0.1:25",
-			//"another-invalid-ip@127.0.0.256",
-			"invalid",
-			"invalid-characters-in-sld@! \"#$%(),/;<>_[]`|.org",
-			//"invalid-ip@127.0.0.1.26",
-			"local-ends-with-dot.@sld.com",
-			"missing-at-sign.net",
-			"missing-sld@.com",
-			"missing-tld@sld.",
-			"sld-ends-with-dash@sld-.com",
-			"sld-starts-with-dashsh@-sld.com",
-			"the-character-limit@for-each-part.of-the-domain.is-sixty-three-characters.this-is-exactly-sixty-four-characters-so-it-is-invalid-blah-blah.com",
-			"the-local-part-is-invalid-if-it-is-longer-than-sixty-four-characters@sld.net",
-			"the-total-length@of-an-entire-address.cannot-be-longer-than-two-hundred-and-fifty-four-characters.and-this-address-is-255-characters-exactly.so-it-should-be-invalid.and-im-going-to-add-some-more-words-here.to-increase-the-lenght-blah-blah-blah-blah-bl.org",
-			"two..consecutive-dots@sld.com",
-			//"unbracketed-IP@127.0.0.1",
+            // examples from wikipedia
+            "Abc.example.com",
+            "A@b@c@example.com",
+            "a\"b(c)d,e:f;g<h>i[j\\k]l@example.com",
+            "just\"not\"right@example.com",
+            "this is\"not\\allowed@example.com",
+            "this\\ still\\\"not\\\\allowed@example.com",
 
-			// examples of real (invalid) input from real users.
-			"No longer available.",
-			"Moved."
-		};
+            // examples from https://github.com/Sembiance/email-validator
+            "! #$%`|@invalid-characters-in-local.org",
+            "(),:;`|@more-invalid-characters-in-local.org",
+            "* .local-starts-with-dot@sld.com",
+            "<>@[]`|@even-more-invalid-characters-in-local.org",
+            "@missing-local.org",
+            "IP-and-port@127.0.0.1:25",
+            //"another-invalid-ip@127.0.0.256",
+            "invalid",
+            "invalid-characters-in-sld@! \"#$%(),/;<>_[]`|.org",
+            //"invalid-ip@127.0.0.1.26",
+            "local-ends-with-dot.@sld.com",
+            "missing-at-sign.net",
+            "missing-sld@.com",
+            "missing-tld@sld.",
+            "sld-ends-with-dash@sld-.com",
+            "sld-starts-with-dashsh@-sld.com",
+            "the-character-limit@for-each-part.of-the-domain.is-sixty-three-characters.this-is-exactly-sixty-four-characters-so-it-is-invalid-blah-blah.com",
+            "the-local-part-is-invalid-if-it-is-longer-than-sixty-four-characters@sld.net",
+            "the-total-length@of-an-entire-address.cannot-be-longer-than-two-hundred-and-fifty-four-characters.and-this-address-is-255-characters-exactly.so-it-should-be-invalid.and-im-going-to-add-some-more-words-here.to-increase-the-lenght-blah-blah-blah-blah-bl.org",
+            "two..consecutive-dots@sld.com",
+            //"unbracketed-IP@127.0.0.1",
 
-		static readonly string[] ValidInternationalAddresses = {
-			"伊昭傑@郵件.商務",    // Chinese
-			"राम@मोहन.ईन्फो",       // Hindi
-			"юзер@екзампл.ком", // Ukranian
-			"θσερ@εχαμπλε.ψομ", // Greek
-		};
+            //ipv6 codecoverage
+            "invalid@[IPv6:123456",
+            "invalid@[IPv6::::FFFF:12345",
+            "invalid.ipv6.addr@[IPv6:2607:f0d0:",
+            "invalid.ipv6.addr@[IPv6:2607:f0d0:1002:5.1::4]",
+            "invalid@[IPv6::FF::FF::0015]",
+            "valid.ipv6.addr@[IPv6:fe80:0000::123.1.72.10:0000:0202:b3ff:fe1e:8329]",
 
-		[Test]
-		public void TestValidAddresses ()
-		{
-			for (int i = 0; i < ValidAddresses.Length; i++)
-				Assert.IsTrue (EmailValidator.Validate (ValidAddresses[i], true), "Valid Address #{0}: {1}", i, ValidAddresses[i]);
-		}
+            // examples of real (invalid) input from real users.
+            "No longer available.",
+            "Moved."
+        };
 
-		[Test]
-		public void TestInvalidAddresses ()
-		{
-			for (int i = 0; i < InvalidAddresses.Length; i++)
-				Assert.IsFalse (EmailValidator.Validate (InvalidAddresses[i], true), "Invalid Address #{0}: {1}", i, InvalidAddresses[i]);
-		}
+        static readonly string[] ValidInternationalAddresses = {
+            "伊昭傑@郵件.商務",    // Chinese
+            "राम@मोहन.ईन्फो",       // Hindi
+            "юзер@екзампл.ком", // Ukranian
+            "θσερ@εχαμπλε.ψομ", // Greek
+        };
 
-		[Test]
-		public void TestValidInternationalAddresses ()
-		{
-			for (int i = 0; i < ValidInternationalAddresses.Length; i++)
-				Assert.IsTrue (EmailValidator.Validate (ValidInternationalAddresses[i], true, true), "Valid International Address #{0}", i);
-		}
+        [Test]
+        public void TestValidAddresses()
+        {
+            for (int i = 0; i < ValidAddresses.Length; i++)
+                Assert.IsTrue(EmailValidator.Validate(ValidAddresses[i], true), "Valid Address #{0}: {1}", i, ValidAddresses[i]);
+        }
 
-		[Test]
-		public void TestThrowsExceptionIfNull ()
-		{
-			Assert.Throws<ArgumentNullException> (() => EmailValidator.Validate (null, true, true), "Null Address");
-		}
-		
-		[Test]
-        	public void TestForNullAttribute ()
-        	{
-            		var target = new InternationalEmailValidationTarget();
-            		target.Email = null;
+        [Test]
+        public void TestInvalidAddresses()
+        {
+            for (int i = 0; i < InvalidAddresses.Length; i++)
+                Assert.IsFalse(EmailValidator.Validate(InvalidAddresses[i], true), "Invalid Address #{0}: {1}", i, InvalidAddresses[i]);
+        }
 
-            		Assert.IsFalse(AreAttributesValid(target), "Address is null");
-        	}
+        [Test]
+        public void TestValidInternationalAddresses()
+        {
+            for (int i = 0; i < ValidInternationalAddresses.Length; i++)
+                Assert.IsTrue(EmailValidator.Validate(ValidInternationalAddresses[i], true, true), "Valid International Address #{0}", i);
+        }
 
-		[Test]
-		public void TestValidationAttributeValidAddresses ()
-		{
-			EmailValidationTarget target = new EmailValidationTarget ();
+        [Test]
+        public void TestThrowsExceptionIfNull()
+        {
+            string mayBeNull = "";
+            Action func = () =>EmailValidator.Validate(mayBeNull, true, true);
+            func();
+            mayBeNull = null;
+            Assert.Throws<ArgumentNullException>(new TestDelegate(func), "Null Address");
+        }
 
-			foreach (var email in ValidAddresses) {
-				target.Email = email;
 
-				Assert.IsTrue (AreAttributesValid (target), "Valid Address {0}", email);
-			}
-		}
+        [Test]
+        public void TestValidationAttributeValidAddresses()
+        {
+            EmailValidationTarget target = new EmailValidationTarget();
 
-		[Test]
-		public void TestValidationAttributeInvalidAddresses ()
-		{
-			EmailValidationTarget target = new EmailValidationTarget ();
+            foreach (var email in ValidAddresses)
+            {
+                target.Email = email;
 
-			foreach (var email in InvalidAddresses) {
-				target.Email = email;
+                Assert.IsTrue(AreAttributesValid(target), "Valid Address {0}", email);
+            }
+        }
 
-				Assert.IsFalse (AreAttributesValid (target), "Invalid Address {0}", email);
-			}
-		}
+        [Test]
+        public void TestThrowsExceptionIfNullAttribute()
+        {
+            var target = new InternationalEmailValidationTarget();
+            target.Email = null;
 
-		[Test]
-		public void TestValidationAttributeValidInternationalAddresses ()
-		{
-			var target = new InternationalEmailValidationTarget ();
+            Assert.IsFalse(AreAttributesValid(target), "Address is null");
+        }
 
-			foreach (var email in ValidInternationalAddresses) {
-				target.Email = email;
+        [Test]
+        public void TestValidationAttributeInvalidAddresses()
+        {
+            EmailValidationTarget target = new EmailValidationTarget();
 
-				Assert.IsTrue (AreAttributesValid (target), "Valid International Address {0}", email);
-			}
-		}
-		
-		[Test]
-		public void TestValidationSkipQuotedWrongStartingCharacter()
-		{
-		    Assert.IsFalse(EmailValidator.Validate("\"é129.185.11.100", false, false));
-		}
+            foreach (var email in InvalidAddresses)
+            {
+                target.Email = email;
 
-		[Test]
-		public void TestValidationSkipQuotedWrongEndCharaecterIndex()
-		{
-		    Assert.IsFalse(EmailValidator.Validate("\"", false, false));
-		}
+                Assert.IsFalse(AreAttributesValid(target), "Invalid Address {0}", email);
+            }
+        }
 
-		bool AreAttributesValid (object target)
-		{
-			var context = new ValidationContext (target, null, null);
-			var results = new List<ValidationResult> ();
+        [Test]
+        public void TestValidationAttributeValidInternationalAddresses()
+        {
+            var target = new InternationalEmailValidationTarget();
 
-			return Validator.TryValidateObject (target, context, results, true);
-		}
+            foreach (var email in ValidInternationalAddresses)
+            {
+                target.Email = email;
 
-		[Test]
-		public void TestInvalidIpAddress()
-		{
-		    string address = "valid.ipv4.addr@[123]";
-		    Assert.IsFalse(EmailValidator.Validate(address, true), "Invalid Address #{0}", address);
-		}
-		
-		[Test]
-        	public void TestForSkippingDomain()
-        	{
-            	    var address = "asd@hu";
-            	    Assert.IsFalse(EmailValidator.Validate(address, false), $"Invalid Address #{0}", address);
-        	}
-		
-		class EmailValidationTarget
-		{
-			[Email (true)]
-			public string Email { get; set; }
-		}
+                Assert.IsTrue(AreAttributesValid(target), "Valid International Address {0}", email);
+            }
+        }
 
-		class InternationalEmailValidationTarget
-		{
-			[Email (true, true)]
-			public string Email { get; set; }
-		}
-	}
+        bool AreAttributesValid(object target)
+        {
+            var context = new ValidationContext(target, null, null);
+            var results = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(target, context, results, true);
+        }
+
+        [Test]
+        public void TestForTopLevelDomainDenial()
+        {
+            var address = "asd@hu";
+            Assert.IsFalse(EmailValidator.Validate(address, false), $"Invalid Address #{0}", address);
+        }
+
+        class EmailValidationTarget
+        {
+            [Email(true)]
+            public string Email { get; set; }
+        }
+
+        class InternationalEmailValidationTarget
+        {
+            [Email(true, true)]
+            public string Email { get; set; }
+        }
+    }
 }
